775be0dfd8679fa9cbba3a64abe9ef077357131c
dvlcube <ulisses@dvlcube.com>
2014-03-09 16:28:45 -0300

    javadoc

--- src/main/java/com/dvlcube/gaming/Updatable.java
+++ src/main/java/com/dvlcube/gaming/Updatable.java
@@ -5,5 +5,12 @@ package com.dvlcube.gaming;
  * @since 27/09/2013
  */
 public interface Updatable {
+
+	/**
+	 * logic stuff.
+	 * 
+	 * @author wonka
+	 * @since 09/03/2014
+	 */
 	void update();
 }
